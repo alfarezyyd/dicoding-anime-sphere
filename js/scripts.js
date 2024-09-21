7ac58011c8889a15368c0b422483202a8946331b
alfarezyyd <adityaalfarezyd@gmail.com>
2024-09-21 20:34:04 +0700

    feat: update

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const darkModeTrigger = document.getElementById("darkModeTrigger");
+const imageTheme = darkModeTrigger.querySelector("img");
+const navbarSelector = document.querySelector(".navbar-container");
+const allAnimeArticle = document.querySelectorAll(".anime-article");
+localStorage.setItem("colorMode", "light-mode")
+darkModeTrigger.addEventListener("click", () => {
+  const colorMode = document.body.classList.contains("light-mode")
+    ? "dark-mode"
+    : "light-mode";
+  document.body.className = colorMode;
+  navbarSelector.classList.remove(localStorage.getItem("colorMode"));
+  navbarSelector.classList.add(colorMode);
+  allAnimeArticle.forEach((article) => {
+    article.classList.remove(localStorage.getItem("colorMode"));
+    article.classList.add(colorMode)
+  })
+  localStorage.setItem("colorMode", colorMode);
+
+  imageTheme.src =
+    colorMode === "dark-mode" ? "./icon/sun.svg" : "./icon/moon.svg";
+});
